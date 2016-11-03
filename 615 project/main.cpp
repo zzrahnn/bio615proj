b0ff9c2fb9d79610539498f9b0f723b00eaec4db
Nina Zhou <ninazhou@ninas-macbook-air.local>
2016-11-03 17:07:13 -0400

    Initial Commit

---
+++ 615 project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  615 project
+//
+//  Created by Nina Zhou on 11/3/16.
+//  Copyright © 2016 Nina Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
