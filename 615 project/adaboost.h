1ed3f2a0bc75beef64abc1442d8754a37d1a4760
dylanfsun <dylanfsun@gmail.com>
2016-11-10 17:16:44 -0500

    added header files

---
+++ 615 project/adaboost.h
@@ -0,0 +1,13 @@
+//
+//  adaboost.h
+//  615 project
+//
+//  Created by Dylan Sun on 11/10/16.
+//  Copyright © 2016 Nina Zhou. All rights reserved.
+//
+
+#ifndef adaboost_h
+#define adaboost_h
+
+
+#endif /* adaboost_h */
---
+++ 615 project/univLogReg.h
@@ -0,0 +1,13 @@
+//
+//  univLogReg.h
+//  615 project
+//
+//  Created by Dylan Sun on 11/10/16.
+//  Copyright © 2016 Nina Zhou. All rights reserved.
+//
+
+#ifndef univLogReg_h
+#define univLogReg_h
+
+
+#endif /* univLogReg_h */
